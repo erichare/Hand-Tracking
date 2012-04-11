b0c56d89a9dee8cecd7b710fed26845e64821f5a
Eric Hare <Raftysworld@gmail.com>
2012-04-11 06:51:53 -0700

    Try to camshift the detected region

--- handTrack.py
+++ handTrack.py
@@ -4,7 +4,61 @@ import sys
 import os
 import string
 
+class App(object):
+    def __init__(self):
+        cv2.namedWindow('camshift')
+        self.tracking = False
+
+    def initialize(self, image, selection):
+        self.image = image.copy()
+        if not self.tracking:
+            self.selection = selection
+
+    def show_hist(self):
+        bin_count = self.hist.shape[0]
+        bin_w = 24
+        img = np.zeros((256, bin_count*bin_w, 3), np.uint8)
+        for i in xrange(bin_count):
+            h = int(self.hist[i])
+            cv2.rectangle(img, (i*bin_w+2, 255), ((i+1)*bin_w-2, 255-h), (int(180.0*i/bin_count), 255, 255), -1)
+        img = cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
+        cv2.imshow('hist', img)
+    
+    def run(self):
+        vis = self.image.copy()
+        hsv = cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, np.array((0, 60, 32), dtype="uint8"), np.array((180, 255, 255), dtype="uint8"))
+
+        if not self.tracking:
+            x0, y0, x1, y1 = self.selection
+            hsv_roi = hsv[y0:y1, x0:x1]
+            mask_roi = mask[y0:y1, x0:x1]
+            hist = cv2.calcHist( [hsv_roi], [0], mask_roi, [16], [0, 180] )
+            cv2.normalize(hist, hist, 0, 255, cv2.NORM_MINMAX);
+            self.hist = hist.reshape(-1)
+            self.show_hist()
+            
+            vis_roi = vis[y0:y1, x0:x1]
+            cv2.bitwise_not(vis_roi, vis_roi)
+            vis[mask == 0] = 0
+            self.tracking = True
+
+        if self.tracking:
+            prob = cv2.calcBackProject([hsv], [0], self.hist, [0, 180], 1)
+            prob &= mask
+            term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 )
+
+            try: track_box, self.selection = cv2.CamShift(prob, self.selection, term_crit)
+            except: "Non-positive sizes"
+            
+            try: cv2.ellipse(vis, track_box, (0, 0, 255), 2)
+            except: print track_box
+                
+        cv2.imshow('camshift', vis)
+
 if __name__ == "__main__":
+    camShifter = App()
+
     image = None
     vc = None
     lst = []
@@ -21,10 +75,15 @@ if __name__ == "__main__":
         lst.append(vc.read()[1])
 
     for f in lst:
-        path = sys.argv[1] + '/' + f
-        if (f == ' '):
-            path = path.rpartition('/')[0]
-        image = cv2.imread(path)
+        image = None
+        if len(sys.argv) > 1:
+            path = sys.argv[1] + '/' + f
+            if (f == ' '):
+                path = path.rpartition('/')[0]
+
+            image = cv2.imread(path)
+        else:
+            image = f
 
         shp = (image.shape[0], image.shape[1])
         hue = np.zeros(shp, dtype='uint8')
@@ -40,17 +99,16 @@ if __name__ == "__main__":
         #cv2.imshow('Hue', hue)
         #cv2.waitKey()
         #cv2.imshow('Saturation', sat)
-        #cv2.waitKey()
         #cv2.imshow('Value', val)
         #cv2.waitKey()
 
-        ret, hue = cv2.threshold(hue, 20, 255, cv2.THRESH_TOZERO) #set to 0 if <= 50, otherwise leave as is
-        ret, hue = cv2.threshold(hue, 234, 255, cv2.THRESH_TOZERO_INV) #set to 0 if > 204, otherwise leave as is
+        ret, hue = cv2.threshold(hue, 50, 255, cv2.THRESH_TOZERO) #set to 0 if <= 50, otherwise leave as is
+        ret, hue = cv2.threshold(hue, 204, 255, cv2.THRESH_TOZERO_INV) #set to 0 if > 204, otherwise leave as is
         ret, hue = cv2.threshold(hue, 0, 255, cv2.THRESH_BINARY_INV) #set to 255 if = 0, otherwise 0
 
-        ret, sat = cv2.threshold(sat, 64, 255, cv2.THRESH_TOZERO) #set to 0 if <= 34, otherwise leave as is
+        ret, sat = cv2.threshold(sat, 64, 255, cv2.THRESH_TOZERO) #set to 0 if <= 64, otherwise leave as is
         sat = cv2.equalizeHist(sat)
-        ret, sat = cv2.threshold(sat, 64, 255, cv2.THRESH_BINARY) #set to 0 if <= 34, otherwise 255
+        ret, sat = cv2.threshold(sat, 64, 255, cv2.THRESH_BINARY) #set to 0 if <= 64, otherwise 255
 
         #cv2.imshow('Saturation threshold', sat)
         #cv2.waitKey()
@@ -65,7 +123,8 @@ if __name__ == "__main__":
 
         #cv2.imshow('Original', image)
         #cv2.waitKey()
-        #cv2.imshow('Hands', hands)
+        cv2.imshow('Hands', hands)
+        cv2.waitKey()
         #cv2.imwrite('out.jpg', hands)
         #cv2.waitKey()
         filteredImage = image.copy()
@@ -81,13 +140,23 @@ if __name__ == "__main__":
                 maxVal.pop()
                 maxVal.append((len(cnt), i))
 
+        rects = []
         for (val, idx) in maxVal:
             if (idx != 0):
                 rect = cv2.boundingRect(contours[idx])
+                newRect = (rect[0], rect[1], rect[0] + rect[2], rect[1] + rect[3])
+                print newRect
+                rects.append(newRect)
                 cv2.rectangle(image, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (255, 0, 0))
 
-        cv2.imshow('Original', image)
-        cv2.waitKey(5)
+        #cv2.imshow('Original', image)
+        #cv2.waitKey()
+
+        if len(rects) > 0 and (rects[0][2] - rects[0][0] > 40) and (rects[0][3] - rects[0][1] > 40):
+            print "Running camshift"
+            camShifter.initialize(image, rects[0])
+            camShifter.run()
+
         if len(sys.argv) == 1:
             lst.append(vc.read()[1])
     
