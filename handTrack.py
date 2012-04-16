6ae25004470e2b8e6361f095451c6b29945d5ac1
Eric Hare <Raftysworld@gmail.com>
2012-04-16 15:11:08 -0700

    Improve camshift and UI

--- handTrack.py
+++ handTrack.py
@@ -8,9 +8,12 @@ class App(object):
     def __init__(self):
         cv2.namedWindow('camshift')
         self.tracking = False
+        self.selection = None
+        self.track_box = None
 
-    def initialize(self, image, selection):
+    def initialize(self, original, image, selection):
         self.image = image.copy()
+        self.original = original.copy()
         if not self.tracking:
             self.selection = selection
 
@@ -25,41 +28,44 @@ class App(object):
         cv2.imshow('hist', img)
     
     def run(self):
-        vis = self.image.copy()
-        hsv = cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV)
-        mask = cv2.inRange(hsv, np.array((0, 60, 32), dtype="uint8"), np.array((180, 255, 255), dtype="uint8"))
+        if self.selection:
+            vis = self.original.copy()
+            hsv = cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV)
+            mask = cv2.inRange(hsv, np.array((0, 60, 32), dtype="uint8"), np.array((180, 255, 255), dtype="uint8"))
 
-        if not self.tracking:
-            x0, y0, x1, y1 = self.selection
-            hsv_roi = hsv[y0:y1, x0:x1]
-            mask_roi = mask[y0:y1, x0:x1]
-            hist = cv2.calcHist( [hsv_roi], [0], mask_roi, [16], [0, 180] )
-            cv2.normalize(hist, hist, 0, 255, cv2.NORM_MINMAX);
-            self.hist = hist.reshape(-1)
-            self.show_hist()
-            
-            vis_roi = vis[y0:y1, x0:x1]
-            cv2.bitwise_not(vis_roi, vis_roi)
-            vis[mask == 0] = 0
-            self.tracking = True
-
-        if self.tracking:
-            prob = cv2.calcBackProject([hsv], [0], self.hist, [0, 180], 1)
-            prob &= mask
-            term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 )
-
-            try: track_box, self.selection = cv2.CamShift(prob, self.selection, term_crit)
-            except: "Non-positive sizes"
-            
-            try: cv2.ellipse(vis, track_box, (0, 0, 255), 2)
-            except: 
-                print track_box
-                self.tracking = False
+            if not self.tracking:
+                x0, y0, x1, y1 = self.selection
+                hsv_roi = hsv[y0:y1, x0:x1]
+                mask_roi = mask[y0:y1, x0:x1]
+                hist = cv2.calcHist( [hsv_roi], [0], mask_roi, [16], [0, 180] )
+                cv2.normalize(hist, hist, 0, 255, cv2.NORM_MINMAX);
+                self.hist = hist.reshape(-1)
+                self.show_hist()
                 
-        cv2.imshow('camshift', vis)
+                vis_roi = vis[y0:y1, x0:x1]
+                cv2.bitwise_not(vis_roi, vis_roi)
+                vis[mask == 0] = 0
+                self.tracking = True
+
+            if self.tracking:
+                prob = cv2.calcBackProject([hsv], [0], self.hist, [0, 180], 1)
+                prob &= mask
+                term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 )
+
+                print "#### CamShift Selection ####"
+                print self.selection
+                print "#### ####"
+
+                try: self.track_box, self.selection = cv2.CamShift(prob, self.selection, term_crit)
+                except: print "FAILED: Non-positive sizes"; self.selection = False; self.tracking = False
+                if (self.track_box[0][1] == 0.0):
+                    self.selection = False; self.tracking = False
 
 if __name__ == "__main__":
-    camShifter = App()
+    camShifter1 = App()
+    camShifter2 = App()
+
+    circles = []
 
     image = None
     vc = None
@@ -102,12 +108,12 @@ if __name__ == "__main__":
         hands = np.zeros(shp, dtype='uint8')
 
         cv2.split(image, (blue, green, red))
-        image[:,:,:][(red > 100) & (green < 100)] = 0   
+        #image[:,:,:][(red > 100) & (green < 100)] = 0   
 
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
         cv2.split(hsv, (hue, sat, val))
 
-        cv2.imshow('Live', image)
+        #cv2.imshow('Live', image)
         #cv2.waitKey()
         #cv2.imshow('Hue', hue)
         #cv2.waitKey()
@@ -115,17 +121,17 @@ if __name__ == "__main__":
         #cv2.imshow('Value', val)
         #cv2.waitKey()
 
-        ret, hue = cv2.threshold(hue, 20, 255, cv2.THRESH_TOZERO) #set to 0 if <= 50, otherwise leave as is
-        ret, hue = cv2.threshold(hue, 234, 255, cv2.THRESH_TOZERO_INV) #set to 0 if > 204, otherwise leave as is
+        ret, hue = cv2.threshold(hue, 50, 255, cv2.THRESH_TOZERO) #set to 0 if <= 50, otherwise leave as is
+        ret, hue = cv2.threshold(hue, 204, 255, cv2.THRESH_TOZERO_INV) #set to 0 if > 204, otherwise leave as is
         ret, hue = cv2.threshold(hue, 0, 255, cv2.THRESH_BINARY_INV) #set to 255 if = 0, otherwise 0
 
         ret, sat = cv2.threshold(sat, 64, 255, cv2.THRESH_TOZERO) #set to 0 if <= 64, otherwise leave as is
         sat = cv2.equalizeHist(sat)
         ret, sat = cv2.threshold(sat, 64, 255, cv2.THRESH_BINARY) #set to 0 if <= 64, otherwise 255
 
-        ret, val = cv2.threshold(val, 20, 255, cv2.THRESH_TOZERO) #set to 0 if <= 50, otherwise leave as is
+        ret, val = cv2.threshold(val, 40, 255, cv2.THRESH_TOZERO) #set to 0 if <= 50, otherwise leave as is
         val = cv2.equalizeHist(val)
-        ret, val = cv2.threshold(val, 20, 255, cv2.THRESH_BINARY) #set to 0 if > 204, otherwise leave as is
+        ret, val = cv2.threshold(val, 40, 255, cv2.THRESH_BINARY) #set to 0 if > 204, otherwise leave as is
 
         #cv2.imshow('Saturation threshold', sat)
         #cv2.waitKey()
@@ -153,10 +159,9 @@ if __name__ == "__main__":
         #cv2.imwrite('out.jpg', hands)
         #cv2.waitKey()
 
-        print filteredImage[1, 1]
-
         contours, hierarchy = cv2.findContours(hands, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
         maxVal = [(0, 0), (0, 0)]
+        #maxVal = [(0, 0)]
         for i in range(len(contours)):
             cnt = contours[i]
             if (maxVal[0][0] < len(cnt) or maxVal[1][0] < len(cnt)):
@@ -164,23 +169,83 @@ if __name__ == "__main__":
                 maxVal.reverse()
                 maxVal.pop()
                 maxVal.append((len(cnt), i))
+            #if (maxVal[0][0] < len(cnt)):
+            #    maxVal = [(len(cnt), i)]
+
+        print "#### MaxVal ####"
+        print maxVal
+        print "#### ####"
 
         rects = []
         for (val, idx) in maxVal:
-            if (idx != 0):
+            if (val != 0):
                 rect = cv2.boundingRect(contours[idx])
                 newRect = (rect[0], rect[1], rect[0] + rect[2], rect[1] + rect[3])
-                print newRect
                 rects.append(newRect)
-                cv2.rectangle(originalImage, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (255, 0, 0))
-
+                cv2.rectangle(originalImage, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (255, 255, 0))
+ 
+        print "#### Rects ####"
+        print rects
+        print "#### ####"
         #cv2.imshow('Original', image)
-        cv2.waitKey()
+        #cv2.waitKey()
 
-        if len(rects) > 0 and (rects[0][2] - rects[0][0] > 40) and (rects[0][3] - rects[0][1] > 40):
-            print "Running camshift"
-            camShifter.initialize(originalImage, rects[0])
-            camShifter.run()
+        if len(rects) > 0:
+            filteredImage1 = filteredImage.copy()
+            ogfilteredImage1 = filteredImage.copy()
+            filteredImage1[rects[0][1] : rects[0][3], rects[0][0] : rects[0][2], :] = 256
+            ogfilteredImage1[filteredImage1 != 256] = 0
+
+        if len(rects) > 1:
+            filteredImage2 = filteredImage.copy()
+            ogfilteredImage2 = filteredImage.copy()
+            filteredImage2[rects[1][1] : rects[1][3], rects[1][0] : rects[1][2], :] = 256
+            ogfilteredImage2[filteredImage2 != 256] = 0
+
+        if len(rects) > 0 and rects[0][2] - rects[0][0] > 20 and rects[0][3] - rects[0][1] > 20:
+            print "Running camshift1"
+            camShifter1.initialize(originalImage, ogfilteredImage1, rects[0])
+
+        if len(rects) > 1 and rects[1][2] - rects[1][0] > 20 and rects[1][3] - rects[1][1] > 20:
+            print "Running camshift2"
+            camShifter2.initialize(originalImage, ogfilteredImage2, rects[1])
+
+        camShifter1.run()
+        camShifter2.run()
+
+        try: cv2.ellipse(originalImage, camShifter1.track_box, (255, 0, 0), 2)
+        except: print "Could not draw ellipse"
+
+        print "#### RotatedRect TrackBox 1 ####"
+        try: print camShifter1.track_box
+        except: print ""
+        print "#### ####"
+
+        try: cv2.ellipse(originalImage, camShifter2.track_box, (0, 0, 255), 2)
+        except: print "Could not draw ellipse"
+
+        print "#### RotatedRect TrackBox 2 ####"
+        try: print camShifter2.track_box
+        except: print ""
+        print "#### ####"
+
+        try: circles.append((int(camShifter1.track_box[0][0]), int(camShifter1.track_box[0][1])))
+        except: print ""
+        if len(circles) > 10000:
+            circles.pop(0)
+        for circ in circles:
+            cv2.circle(image, circ, 3, (0, 255, 0), -1)
+
+        try: circles.append((int(camShifter2.track_box[0][0]), int(camShifter2.track_box[0][1])))
+        except: print ""
+        if len(circles) > 10000:
+            circles.pop(0)
+        for circ in circles:
+            cv2.circle(image, circ, 3, (255, 0, 255), -1)
+                    
+        cv2.imshow('Live', image)
+        cv2.imshow('camshift', originalImage)
+        cv2.waitKey(5)
 
         if len(sys.argv) == 1:
             lst.append(vc.read()[1])
